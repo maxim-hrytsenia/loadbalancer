ebce23b5fb1d0c4c2d7868a6c2a1928665459c2d
Maksim Hrytsenia <maximhrytsenia@gmail.com>
2022-02-06 17:14:27 +0100

    finish first task, add loadbalancer, add config

--- src/LoadBalancer.py
+++ src/LoadBalancer.py
@@ -1,3 +1,11 @@
 class LoadBalancer:
-    def __init__(self):
-        pass
+    def __init__(self, servers):
+        self.servers = servers
+        self.current_server = 0
+        self.n_servers = len(servers)
+
+
+    def send_request(self):
+        response =  self.servers[self.current_server]
+        self.current_server = (self.current_server + 1) % self.n_servers
+        return response
