3a97bff5ed2f72068514dee995a12302c818bc03
Maksim Hrytsenia <maximhrytsenia@gmail.com>
2022-02-08 20:11:28 +0100

    modify main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,48 @@
 from LoadBalancer import LoadBalancer
 from config import server_ids
+from AdvancedLoadBalancer import AdvancedLoadBalancer
+import time
 
 
 def main():
-    lb = LoadBalancer(server_ids)
+    lb = AdvancedLoadBalancer(server_ids)
     print(lb.send_request())
     print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.send_request())
+    print(lb.load)
+    time.sleep(1)
+    print(lb.load)
+    time.sleep(1)
+    print(lb.load)
+    time.sleep(1)
+    print(lb.load)
+    time.sleep(1)
+    print(lb.load)
+    time.sleep(1)
+    print(lb.load)
+    time.sleep(1)
+    print(lb.load)
+    time.sleep(1)
+    print(lb.load)
+    time.sleep(1)
+    print(lb.load)
 
 
 if __name__ == "__main__":
