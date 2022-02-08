d13b2ae2e5c01759064857ade2660e86931bee54
Maksim Hrytsenia <maximhrytsenia@gmail.com>
2022-02-08 21:53:23 +0100

    add new testing method for load balancer, refactor testing class

--- src/tests/test_load_balancers.py
+++ src/tests/test_load_balancers.py
@@ -1,15 +1,17 @@
 import os, sys
+
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 from collections import Counter
 from advanced_load_balancer import AdvancedLoadBalancer
+from load_balancer import LoadBalancer
 from config import server_ids
 import time
 
 
-class TestAdvancedLoadBalancer:
-    def test_distribution(self):
+class TestLoadBalancers:
+    def test_advanced_lb_distribution(self):
         lb = AdvancedLoadBalancer(server_ids)
         number_of_requests = 100000
         counter = Counter()
@@ -26,7 +28,7 @@ class TestAdvancedLoadBalancer:
         assert(abs(max_requests - expected_requests) / expected_requests < 0.01)
 
 
-    def test_requests_cleaning(self):
+    def test_advanced_lb_cleaning(self):
         lb = AdvancedLoadBalancer(server_ids)
         number_of_requests = 100000
         for _ in range(number_of_requests):
@@ -34,4 +36,18 @@ class TestAdvancedLoadBalancer:
         assert(sum(lb.load) > 0)
         time.sleep(10)
         assert(sum(lb.load) == 0)
+
+
+    def test_lb_distribution(self):
+        lb = LoadBalancer(server_ids)
+        number_of_requests = 100000
+        counter = Counter()
+        for _ in range(number_of_requests):
+            counter[lb.send_request()] += 1
+        # check that load is distributed to all servers
+        assert(len(counter) == len(server_ids))
+        min_requests = min(counter.values())
+        max_requests = max(counter.values())
+        # check that the load in count between min and max loaded server is no more than 1
+        assert(max_requests - min_requests <= 1)
     
